56ca5293c17ecd988e18e4921e1592d9c33bae15
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-06-22 11:57:39 +0300

    Payment

--- app/src/main/java/com/example/agrican/ui/screens/home/agricanservices/order/order_status/OrderStatusScreen.kt
+++ app/src/main/java/com/example/agrican/ui/screens/home/agricanservices/order/order_status/OrderStatusScreen.kt
@@ -1,7 +1,9 @@
 package com.example.agrican.ui.screens.home.agricanservices.order.order_status
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
@@ -27,7 +29,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,8 +41,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.agrican.R
-import com.example.agrican.common.snackbar.SnackBarManager
-import com.example.agrican.common.snackbar.SnackBarMessage
 import com.example.agrican.domain.model.Order
 import com.example.agrican.ui.components.BackButton
 import com.example.agrican.ui.components.EmptyImage
@@ -55,8 +54,7 @@ import com.example.agrican.ui.theme.title
 import com.paymob.acceptsdk.IntentConstants
 import com.paymob.acceptsdk.PayResponseKeys
 import com.paymob.acceptsdk.SaveCardResponseKeys
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import com.paymob.acceptsdk.ToastMaker
 
 object OrderStatusDestination: NavigationDestination {
     override val route: String = "order_status"
@@ -219,7 +217,6 @@ fun OrdersListItem(
         }
 
         val context = LocalContext.current
-        val coroutineScope = rememberCoroutineScope()
         val paymentLauncher = rememberLauncherForActivityResult(
             contract = ActivityResultContracts.StartActivityForResult(),
             onResult = { result ->
@@ -228,70 +225,76 @@ fun OrdersListItem(
                 when (result.resultCode) {
                     IntentConstants.USER_CANCELED -> {
                         // User canceled and did no payment request was fired
-                        showSnackBarMessage(coroutineScope, "User canceled!!")
+                        ToastMaker.displayShortToast(context as Activity, "User canceled!!")
                     }
                     IntentConstants.MISSING_ARGUMENT -> {
                         // You forgot to pass an important key-value pair in the intent's extras
-                        showSnackBarMessage(
-                            coroutineScope,
-                            "Missing Argument == " +
-                                    extras!!.getString(IntentConstants.MISSING_ARGUMENT_VALUE)
-                            )
+                        ToastMaker.displayShortToast(
+                            context as Activity,
+                            "Missing Argument == " + extras!!.getString(IntentConstants.MISSING_ARGUMENT_VALUE)
+                        )
                     }
                     IntentConstants.TRANSACTION_ERROR -> {
                         // An error occurred while handling an APIs response
-                        showSnackBarMessage(
-                            coroutineScope,
+                        ToastMaker.displayShortToast(
+                            context as Activity,
                             "Reason == " + extras!!.getString(IntentConstants.TRANSACTION_ERROR_REASON)
                         )
                     }
                     IntentConstants.TRANSACTION_REJECTED -> {
                         // User attempted to pay but their transaction was rejected
-                        showSnackBarMessage(coroutineScope, "REJECTED")
+                        Toast.makeText(context, "REJECTED", Toast.LENGTH_SHORT).show()
                         // Use the static keys declared in PayResponseKeys to extract the fields you want
-                        extras!!.getString(PayResponseKeys.DATA_MESSAGE)
-                            ?.let { showSnackBarMessage(coroutineScope, it) }
+                        ToastMaker.displayShortToast(context as Activity, extras!!.getString(PayResponseKeys.DATA_MESSAGE))
                     }
                     IntentConstants.TRANSACTION_REJECTED_PARSING_ISSUE -> {
                         // User attempted to pay but their transaction was rejected. An error occurred while reading the returned JSON
-                        extras!!.getString(IntentConstants.RAW_PAY_RESPONSE)
-                            ?.let { showSnackBarMessage(coroutineScope, it) }
+                        ToastMaker.displayShortToast(
+                            context as Activity,
+                            extras!!.getString(IntentConstants.RAW_PAY_RESPONSE)
+                        )
                     }
                     IntentConstants.TRANSACTION_SUCCESSFUL -> {
                         // User finished their payment successfully
-                        showSnackBarMessage(coroutineScope, "SUCCESS")
+                        Toast.makeText(context, "SUCCESS", Toast.LENGTH_SHORT).show()
 
                         // Use the static keys declared in PayResponseKeys to extract the fields you want
-                        extras!!.getString(PayResponseKeys.DATA_MESSAGE)
-                            ?.let { showSnackBarMessage(coroutineScope, it) }
+                        ToastMaker.displayShortToast(context as Activity, extras!!.getString(PayResponseKeys.DATA_MESSAGE))
                     }
                     IntentConstants.TRANSACTION_SUCCESSFUL_PARSING_ISSUE -> {
                         // User finished their payment successfully. An error occurred while reading the returned JSON.
-                        showSnackBarMessage(coroutineScope, "TRANSACTION_SUCCESSFUL - Parsing Issue")
+                        ToastMaker.displayShortToast(context as Activity, "TRANSACTION_SUCCESSFUL - Parsing Issue")
                     }
                     IntentConstants.TRANSACTION_SUCCESSFUL_CARD_SAVED -> {
                         // User finished their payment successfully and card was saved.
-                        showSnackBarMessage(coroutineScope, "SUCCESS CARD SAVE")
+                        Toast.makeText(context as Activity, "SUCCESS CARD SAVE", Toast.LENGTH_SHORT).show()
 
                         // Use the static keys declared in PayResponseKeys to extract the fields you want
                         // Use the static keys declared in SaveCardResponseKeys to extract the fields you want
-                        showSnackBarMessage(coroutineScope, "Token == " + extras!!.getString(SaveCardResponseKeys.TOKEN))
+                        ToastMaker.displayShortToast(
+                            context,
+                            "Token == " + extras!!.getString(SaveCardResponseKeys.TOKEN)
+                        )
                     }
                     IntentConstants.USER_CANCELED_3D_SECURE_VERIFICATION -> {
-                        showSnackBarMessage(coroutineScope, "User canceled 3-d secure verification!!")
+                        ToastMaker.displayShortToast(context as Activity, "User canceled 3-d secure verification!!")
 
                         // Note that a payment process was attempted. You can extract the original returned values
                         // Use the static keys declared in PayResponseKeys to extract the fields you want
-                        extras!!.getString(PayResponseKeys.PENDING)
-                            ?.let { showSnackBarMessage(coroutineScope, it) }
+                        ToastMaker.displayShortToast(context, extras!!.getString(PayResponseKeys.PENDING))
                     }
                     IntentConstants.USER_CANCELED_3D_SECURE_VERIFICATION_PARSING_ISSUE -> {
-                        showSnackBarMessage(coroutineScope, "User canceled 3-d secure verification - Parsing Issue!!")
+                        ToastMaker.displayShortToast(
+                            context as Activity,
+                            "User canceled 3-d secure verification - Parsing Issue!!"
+                        )
 
                         // Note that a payment process was attempted.
                         // User finished their payment successfully. An error occurred while reading the returned JSON.
-                        extras!!.getString(IntentConstants.RAW_PAY_RESPONSE)
-                            ?.let { showSnackBarMessage(coroutineScope, it) }
+                        ToastMaker.displayShortToast(
+                            context,
+                            extras!!.getString(IntentConstants.RAW_PAY_RESPONSE)
+                        )
                     }
                 }
             }
@@ -307,12 +310,6 @@ fun OrdersListItem(
                 text = stringResource(id = R.string.confirm_order),
             )
         }
-    }
-}
-
-private fun showSnackBarMessage(coroutineScope: CoroutineScope, message: String) {
-    coroutineScope.launch {
-        SnackBarManager.showMessage(SnackBarMessage.StringSnackBar(message))
     }
 }
 
