7e9fdaf46d583cdb8805fca99a1a7350b265905a
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-08-01 20:42:14 +0300

    FCM

--- app/src/main/java/com/theflankers/agrican/MainActivity.kt
+++ app/src/main/java/com/theflankers/agrican/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.theflankers.agrican
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +15,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.amplifyframework.auth.AuthException
 import com.amplifyframework.kotlin.core.Amplify
+import com.google.android.material.snackbar.Snackbar
 import com.theflankers.agrican.ui.AgricanApp
 import com.theflankers.agrican.ui.screens.splash.SplashViewModel
 import com.theflankers.agrican.ui.theme.AgricanTheme
@@ -24,8 +31,40 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            Snackbar.make(
+                findViewById<View>(android.R.id.content).rootView,
+                "Please grant Notification permission from App Settings",
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        askNotificationPermission()
 
         // Fetch Amplify Session
         lifecycleScope.launch {
