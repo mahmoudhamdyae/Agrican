466bbf2312ee7d98d8eeae9a568de6872a9f8b41
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-08-08 18:20:33 +0300

    Google Analytics

--- app/src/main/java/com/theflankers/agrican/AgricanApplication.kt
+++ app/src/main/java/com/theflankers/agrican/AgricanApplication.kt
@@ -5,13 +5,20 @@ import android.util.Log
 import com.amplifyframework.AmplifyException
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.kotlin.core.Amplify
+import com.google.android.gms.analytics.GoogleAnalytics
+import com.google.android.gms.analytics.Tracker
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AgricanApplication: Application() {
 
+    private var sAnalytics: GoogleAnalytics? = null
+    private var sTracker: Tracker? = null
+
     override fun onCreate() {
         super.onCreate()
+
+        sAnalytics = GoogleAnalytics.getInstance(this);
 
         // Install Amplify
         try {
@@ -21,5 +28,18 @@ class AgricanApplication: Application() {
         } catch (error: AmplifyException) {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
         }
+    }
+
+    /**
+     * Gets the default [Tracker] for this [Application].
+     * @return tracker
+     */
+    @Synchronized
+    fun getDefaultTracker(): Tracker? {
+        // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+        if (sTracker == null) {
+            sTracker = sAnalytics?.newTracker(R.xml.global_tracker)
+        }
+        return sTracker
     }
 }
